d8e606a43a4f376526dacfe409edecd9da5529d8
Tomas Jansson <tomas.jansson@gmail.com>
2013-05-02 21:50:02 +0200

    initial commit

---
+++ AccessControlDemo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccessControlDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var ci = Thread.CurrentPrincipal.Identity as System.Security.Claims.ClaimsIdentity;
+            var claims = Enumerable.Empty<Claim>();
+            if (ci != null && ci.Claims != null)
+            {
+                claims = ci.Claims.AsEnumerable();
+            }
+            return View(claims);
+        }
+
+    }
+}
---
+++ AccessControlDemo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AccessControlDemo2.MvcApplication" Language="C#" %>
